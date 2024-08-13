d4ad93acac784f402d78327213a53a6671a5e472
Peter Greshnov <greshnov.peter@gmail.com>
2024-08-13 21:16:41 +0600

    S2L26: Type Aliases & Object Types

---
+++ s2/s2l26_union_aliases.ts
@@ -0,0 +1,44 @@
+// S2L23 - Union type
+
+function combine(input1: number | string | boolean, input2: number | string) {
+  let result: number | string;
+  if (typeof input1 === "number" && typeof input2 === "number") {
+    result = input1 + input2;
+  } else {
+    // result = JSON.stringify(input1).toString() + JSON.stringify(input2).toString();
+    // result = JSON.stringify(input1) + JSON.stringify(input2);
+    result = input1.toString() + input2.toString();
+  }
+  return result;
+}
+
+console.log(combine(1, 2));
+// console.log(combine("Peter", "Kate"));
+// console.log(combine(true, "Kate"));
+
+// S2L24 - Literal types
+
+// Type Aliases:
+type Combinable = number | string;
+type resultConversion = "as-number" | "as-text";
+
+function combine2(input1: Combinable | boolean, input2: Combinable, forceConversion: resultConversion) {
+  let result: number | string;
+  if ((typeof input1 === "number" && typeof input2 === "number") || forceConversion === "as-number") {
+    result = +input1 + +input2;
+  } else {
+    // result = JSON.stringify(input1).toString() + JSON.stringify(input2).toString();
+    // result = JSON.stringify(input1) + JSON.stringify(input2);
+    result = input1.toString() + input2.toString();
+  }
+  // if (forceConversion === "as-number") return +result;
+  // else return result.toString();
+  return result;
+}
+
+console.log(combine2(1, 2, "as-number"));
+console.log(combine2("1", "2", "as-number"));
+console.log(combine2("1", "2", "as-text"));
+console.log(combine2("Peter", "Kate", "as-text"));
+
+// S2L24 - Type Aliases & Object Types
