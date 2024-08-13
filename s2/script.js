8a932fcb57b0427b59d7bbcb59edb54db72f53ec
Peter Greshnov <greshnov.peter@gmail.com>
2024-08-13 21:57:53 +0600

    S2L8 Functions as types

--- s2/script.js
+++ s2/script.js
@@ -9,3 +9,12 @@ function printResult(num) {
     console.log("Result: ".concat(num));
 }
 printResult(add(1, 2));
+// S2L8 Functions as types;
+// That is a very generic type - Function - nothging is known about args or return type
+// let combineValues: Function;
+// This is a specific function type (no need to match parameters names):
+var combineValues;
+combineValues = add;
+// combineValues = printResult; // that throws an error;
+// combineValues = 5; // that is an Error;
+console.log(combineValues(8, 8));
--- s2/script.ts
+++ s2/script.ts
@@ -13,3 +13,17 @@ function printResult(num: number): void {
 }
 
 printResult(add(1, 2));
+
+// S2L8 Functions as types;
+
+// That is a very generic type - Function - nothging is known about args or return type
+// let combineValues: Function;
+
+// This is a specific function type (no need to match parameters names):
+let combineValues: (a: number, b: number) => number;
+
+combineValues = add;
+// combineValues = printResult; // that throws an error;
+// combineValues = 5; // that is an Error;
+
+console.log(combineValues(8, 8));
