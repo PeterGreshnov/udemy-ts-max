5e860c4c0c9f199781950a5e606929991a443b8a
Peter Greshnov <greshnov.peter@gmail.com>
2024-08-14 16:17:02 +0600

    S3L39: TS Core Libs

--- s3/script.js
+++ s3/script.js
@@ -2,3 +2,10 @@
 // S3L35: Watch mode
 // S3L36: Compiling multiple .ts files
 console.log("Script file...");
+// S3L37: Including and excluding files
+// S3L39: TS Core Libs
+const button = document.querySelector("button");
+console.log(button);
+button === null || button === void 0 ? void 0 : button.addEventListener("click", () => {
+    console.log("clicked!");
+});
--- s3/script.ts
+++ s3/script.ts
@@ -2,3 +2,13 @@
 
 // S3L36: Compiling multiple .ts files
 console.log("Script file...");
+
+// S3L37: Including and excluding files
+
+// S3L39: TS Core Libs
+
+const button = document.querySelector("button");
+console.log(button);
+button?.addEventListener("click", () => {
+  console.log("clicked!");
+});
