cf1f68fe6996ec9f7e2a9663ff8c86371f1a216f
Peter Greshnov <greshnov.peter@gmail.com>
2024-08-14 15:43:42 +0600

    S3L36: Compiling multiple .ts files

---
+++ s3/script.js
@@ -0,0 +1,4 @@
+"use strict";
+// S3L35: Watch mode
+// S3L36: Compiling multiple .ts files
+console.log("Script file...");
---
+++ s3/script.ts
@@ -0,0 +1,4 @@
+// S3L35: Watch mode
+
+// S3L36: Compiling multiple .ts files
+console.log("Script file...");
