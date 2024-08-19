860ba8e10359e5dcce7c07430c9feec4f5f84c21
Peter Greshnov <peter.greshnov@saritasa.com>
2024-08-19 22:02:02 +0600

    S5L74 Interfaces with classes

--- s5/src/script.ts
+++ s5/src/script.ts
@@ -1,18 +1,41 @@
 // S5L73 Interfaces
 
-interface Person {
-  name: string;
-  age: number;
+// interface Person {
+//   name: string;
+//   age: number;
 
+//   greet(phrase: string): void;
+// }
+
+// const user1: Person = {
+//   age: 36,
+//   name: "Peter",
+//   greet(phrase) {
+//     console.log(phrase + " " + this.name);
+//   },
+// };
+
+// user1.greet("Hi, I'm");
+
+// S5L74 Interfaces with classes
+
+interface Greetable {
+  name: string;
   greet(phrase: string): void;
 }
 
-const user1: Person = {
-  age: 36,
-  name: "Peter",
-  greet(phrase) {
-    console.log(phrase + " " + this.name);
-  },
-};
+class Person implements Greetable {
+  name: string;
+  age = 30;
 
-user1.greet("Hi, I'm");
+  constructor(n: string) {
+    this.name = n;
+  }
+
+  greet(phrase: string): void {
+    console.log(`${phrase} ${this.name}`);
+  }
+}
+
+const user1 = new Person("Peter");
+console.log(user1);
