86d25a1d8ac831bb168eaed4ecd322451e61b89e
Peter Greshnov <peter.greshnov@saritasa.com>
2024-08-20 15:17:04 +0600

    S56L85 Type guards

--- s6/src/script.ts
+++ s6/src/script.ts
@@ -35,3 +35,68 @@ type Universal = Combinable & Numeric; // here in case of union types - it works
 // const uni: Universal = true; // error
 const uni: Universal = 10; // ok
 console.log(uni);
+
+// S56L85 Type guards
+
+function add(a: Combinable, b: Combinable) {
+  //1. type-guard using "typeof":
+  if (typeof a === "string" || typeof b === "string") {
+    return a.toString() + b.toString();
+  }
+  return a + b;
+}
+
+console.log(add(1, 3));
+
+type UnknownEmployee = Employee | Admin;
+
+function printEmployeeInfo(emp: UnknownEmployee) {
+  console.log(`Name: ${emp.name}`);
+
+  //2. Checking if obj has a property
+  // console.log(`Privs: ${emp.priveleges}`); //  error without a type-guard;
+  if ("priveleges" in emp) console.log(`Privs: ${emp.priveleges}`);
+  if ("startDate" in emp) console.log(`Start Date: ${emp.startDate}`);
+}
+
+printEmployeeInfo(e1);
+printEmployeeInfo({ name: "John", startDate: new Date() });
+
+// 3. "instanceof" type guard
+
+class Car {
+  drive() {
+    console.log("Driving...");
+  }
+}
+
+class Truck {
+  drive() {
+    console.log("Driving a truck");
+  }
+
+  loadCargo(amount: number) {
+    console.log(`Loading cargo ... ${amount}`);
+  }
+}
+
+type Vehicle = Car | Truck;
+
+const v1 = new Car();
+const v2 = new Truck();
+
+function useVehicle(vehicle: Vehicle) {
+  vehicle.drive();
+  // option 1:
+  if ("loadCargo" in vehicle) {
+    vehicle.loadCargo(12);
+  }
+
+  // option 2:
+  if (vehicle instanceof Truck) {
+    vehicle.loadCargo(23);
+  }
+}
+
+useVehicle(v1);
+useVehicle(v2);
