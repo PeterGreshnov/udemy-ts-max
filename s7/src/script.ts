c10c356629d73c0d50715e8562b64947d41f0116
Peter Greshnov <greshnov.peter@gmail.com>
2024-08-22 22:23:44 +0600

    S7L97 Working with Type constrains

--- s7/src/script.ts
+++ s7/src/script.ts
@@ -33,3 +33,17 @@ const mergedObj2 = merge<{ name: string; hobbies: string[] }, { age: number }>( 
 console.log(mergedObj.name); // can't access name \ age when not using generic function with <T, U>
 console.log(mergedObj);
 console.log(mergedObj2);
+
+// S7L97 Working with Type constrains
+
+const mergedObj3 = merge({ name: "Peter", hobbies: ["guitar"] }, 36);
+console.log(mergedObj3); // 36 is not in the object;
+// type constrains are applied with "extends"
+
+function mergeWConstr<T extends object, U extends object>(objA: T, objB: U) {
+  return Object.assign(objA, objB);
+}
+
+// const mergedObj4 = mergeWConstr({ name: "Peter" }, 25); // with constrain ("extends object") that doesn't work
+const mergedObj4 = mergeWConstr({ name: "Peter" }, { age: 25 });
+console.log(mergedObj4);
