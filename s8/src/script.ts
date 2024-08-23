c57a52b6d954f6cb823a33ce634089697a687bc3
Peter Greshnov <greshnov.peter@gmail.com>
2024-08-23 17:09:51 +0600

    S8L109 Adding multiple Decorators

--- s8/src/script.ts
+++ s8/src/script.ts
@@ -17,6 +17,7 @@ function LoggerFactory(logString: string) {
 function WithTemplate(template: string, hookId: string) {
   // eslint-disable-next-line @typescript-eslint/no-unsafe-function-type
   return function (constructor: new () => Person) {
+    console.log("Rendering template...");
     const hookEl = document.getElementById(hookId);
     const p = new constructor();
     if (hookEl) {
@@ -27,8 +28,8 @@ function WithTemplate(template: string, hookId: string) {
 }
 
 // @Logger // class decorators are executed when the class is defined, not when it is instasiated;
-// @LoggerFactory("LOGGGING _ PERSON")
-@WithTemplate("<h1>MY PERSON OBJECT</h1>", "app")
+@LoggerFactory("LOGGGING _ PERSON") // this one runs 2nd
+@WithTemplate("<h1>MY PERSON OBJECT</h1>", "app") // decorator functions are executed bottom to top - this on runs 1st
 class Person {
   name = "Peter";
 
@@ -44,3 +45,5 @@ console.log(pers);
 // S8L107 Decorator Factories
 
 // S8L108 More useful Decorators
+
+// S8L109 Adding multiple Decorators
