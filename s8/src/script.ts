1c0f42f31da48090f6329eb5944a9c111790ce32
Peter Greshnov <greshnov.peter@gmail.com>
2024-08-24 15:35:52 +0600

    S8L111 Accessor & Parameter Decorators

--- s8/src/script.ts
+++ s8/src/script.ts
@@ -47,3 +47,57 @@ console.log(pers);
 // S8L108 More useful Decorators
 
 // S8L109 Adding multiple Decorators
+
+// S8L110 Property Decorators
+
+function Log(target: any, propertyName: string | symbol) {
+  console.log("Prop decorator!");
+  console.log(target, propertyName);
+}
+
+function Log2(target: any, propertyName: string, propertyDescriptor: PropertyDescriptor) {
+  console.log("Accessor decorator!");
+  console.log(`target`, target);
+  console.log(`propertyName`, propertyName);
+  console.log(`propertyDescriptor`, propertyDescriptor);
+}
+
+function Log3(target: any, propertyName: string | symbol, propertyDescriptor: PropertyDescriptor) {
+  console.log("METHOD decorator!");
+  console.log(`target`, target);
+  console.log(`propertyName`, propertyName);
+  console.log(`propertyDescriptor`, propertyDescriptor);
+}
+
+function Log4(target: any, propertyName: string | symbol, position: number) {
+  console.log("Parameter decorator!");
+  console.log(`target`, target);
+  console.log(`propertyName`, propertyName);
+  console.log(`position`, position);
+}
+
+class Product {
+  @Log
+  title: string;
+  private _price: number;
+
+  constructor(t: string, p: number) {
+    this.title = t;
+    this._price = p;
+  }
+
+  @Log2
+  set price(val: number) {
+    if (val > 0) this._price = val;
+    else {
+      throw new Error("Price should be positive");
+    }
+  }
+
+  @Log3
+  getPriceWithTax(@Log4 tax: number) {
+    return this._price * (1 + tax);
+  }
+}
+
+// S8L111 Accessor & Parameter Decorators
