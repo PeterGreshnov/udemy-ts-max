be9acf4e829f1fd3a5420b5ae034a310c351daf4
Peter Greshnov <greshnov.peter@gmail.com>
2024-08-27 13:46:36 +0600

    S9L126 Fetching user input

--- s9/src/app.ts
+++ s9/src/app.ts
@@ -28,10 +28,35 @@ class ProjectInput {
     this.attach();
   }
 
+  private getUserInput(): [string, string, number] | void {
+    const inputTitle = this.titleInputEl.value;
+    const inputDescription = this.descriptionInputEl.value;
+    const inputPeople = this.peopleInputEl.value;
+
+    if (inputTitle.trim().length === 0 || inputDescription.trim().length === 0 || inputPeople.trim().length === 0) {
+      // throw new Error("Invalid Inputs");
+      alert("Invalid Input, pls try again!");
+      return;
+    } else {
+      return [inputTitle, inputDescription, +inputPeople];
+    }
+  }
+
+  private clearInputs() {
+    this.titleInputEl.value = "";
+    this.descriptionInputEl.value = "";
+    this.peopleInputEl.value = "";
+  }
+
   private submitHandler(e: Event) {
     e.preventDefault();
     console.log("FORM SUBMITTED!");
-    console.log(this.titleInputEl.value);
+    const userInput = this.getUserInput();
+    if (Array.isArray(userInput)) {
+      const [title, description, people] = userInput;
+      console.log(title, description, people);
+      this.clearInputs();
+    }
   }
 
   private configure() {
@@ -47,3 +72,5 @@ const projectInput = new ProjectInput();
 console.log(projectInput);
 
 // S9L124 Interacting with DOM elements
+
+// S9L126 Fetching user input
