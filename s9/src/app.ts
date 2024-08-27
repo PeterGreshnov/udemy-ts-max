62d8904b9d8e61eb868f86e537169d9685ed56b3
Peter Greshnov <greshnov.peter@gmail.com>
2024-08-27 12:49:27 +0600

    S9L123 DOM ELs selection & OOP rendering

--- s9/src/app.ts
+++ s9/src/app.ts
@@ -1,1 +1,24 @@
 // S9L122 Drag and drop start
+
+// S9L123 DOM ELs selection & OOP rendering
+class ProjectInput {
+  templateEl: HTMLTemplateElement;
+  hostEl: HTMLDivElement;
+  element: HTMLFormElement;
+
+  constructor() {
+    this.templateEl = document.getElementById("project-input")! as HTMLTemplateElement;
+    this.hostEl = document.getElementById("app")! as HTMLDivElement;
+
+    const importedNode = document.importNode(this.templateEl.content, true);
+    this.element = importedNode.firstElementChild as HTMLFormElement;
+    this.attach();
+  }
+
+  private attach() {
+    this.hostEl.insertAdjacentElement("afterbegin", this.element);
+  }
+}
+
+const projectInput = new ProjectInput();
+console.log(projectInput);
