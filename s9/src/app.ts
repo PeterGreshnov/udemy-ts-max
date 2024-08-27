5de2810cdde3b833ed7b5947812156b48c4d38f4
Peter Greshnov <greshnov.peter@gmail.com>
2024-08-27 13:31:52 +0600

    S9L124 Interacting with DOM elements

--- s9/src/app.ts
+++ s9/src/app.ts
@@ -6,13 +6,36 @@ class ProjectInput {
   hostEl: HTMLDivElement;
   element: HTMLFormElement;
 
+  titleInputEl: HTMLInputElement;
+  descriptionInputEl: HTMLInputElement;
+  peopleInputEl: HTMLInputElement;
+
   constructor() {
     this.templateEl = document.getElementById("project-input")! as HTMLTemplateElement;
     this.hostEl = document.getElementById("app")! as HTMLDivElement;
 
     const importedNode = document.importNode(this.templateEl.content, true);
     this.element = importedNode.firstElementChild as HTMLFormElement;
+    this.element.id = "user-input";
+
+    this.titleInputEl = this.element.querySelector("#title") as HTMLInputElement;
+    this.descriptionInputEl = this.element.querySelector("#description") as HTMLInputElement;
+    this.peopleInputEl = this.element.querySelector("#people") as HTMLInputElement;
+
+    console.log(this.titleInputEl, this.descriptionInputEl, this.peopleInputEl);
+
+    this.configure();
     this.attach();
+  }
+
+  private submitHandler(e: Event) {
+    e.preventDefault();
+    console.log("FORM SUBMITTED!");
+    console.log(this.titleInputEl.value);
+  }
+
+  private configure() {
+    this.element.addEventListener("submit", this.submitHandler.bind(this));
   }
 
   private attach() {
@@ -22,3 +45,5 @@ class ProjectInput {
 
 const projectInput = new ProjectInput();
 console.log(projectInput);
+
+// S9L124 Interacting with DOM elements
